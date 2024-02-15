d92ee110bf9c479bb994cca944385835228b9348
abood <abdbinm55@gmail.com>
2024-02-15 13:17:20 +0300

    program done

--- employeeManagementSystem.py
+++ employeeManagementSystem.py
@@ -13,19 +13,82 @@ class Employee:
         
     def setId(self, id):
         self.id = id
-        
-    def getId(self):
-        return self.id
-        
+                
     def setDepartment(self, department):
         self.department = department
+
+    def getId(self):
+        return self.id
+    
+    def getAge(self):
+        return self.age
+    
+    def getDepartment(self):
+        return self.department
         
+    def __str__(self) -> str:
+        return f"Employee ID: {self.id}\n Employee Name: {self.name}\n Employee Age:{self.age}\n Employee Department: {self.department}"
+    
 class EMS:
     def __init__(self) -> None:
         self.employeeList = []
         
-    def addNewEmployee(self, name, age, id, department):
-        self.employeeList.append({Employee(name, age, id, department)})
+    def addNewEmployee(self, name, age, department):
+        self.employeeList.append(Employee(name, age,len(self.employeeList)+1,department))
         
+    def getEmployeeInfo(self, id):
+        for i in self.employeeList:
+            if id == i.getId():
+                return i
+            
+    def deleteEmployeeInfo(self, id):
+        for i in self.employeeList:
+            if id == i.getId():
+                self.employeeList.remove(i)
+            
+def main():
+    obj = EMS()
+    while True:
+        print("Choose one of the following options:")
+        inpt = input("\tType 1 for adding new employee\n\tType 2 for getting an employee by the ID\n\tType 3 for deleting an employee by the ID\n\tType q to quit: ").replace(" ", "")
+        
+        if inpt == "1":
+            name = input("Enter the employee name: ")
+            age = input("Enter the employee age: ")
+            dpt = input("Enter the employee department: ")
+            if not (name.isalpha() and age.isdigit() and dpt.isalpha()):
+                print("\nPlease check your input\n")
+                continue
+            if int(age) < 19:
+                print("\nPlease check your input\n")
+                continue
+            obj.addNewEmployee(name, int(age), dpt)
+            print("\nA new employee have been added succefully!")
+            print("\n---------------------------------------------\n")
+        
+        if inpt == "2":
+            id = input("Enter the employee ID: ")
+            if id.isalpha() or id=="":
+                print("\nPlease check your input\n")
+                continue
+            res = obj.getEmployeeInfo(int(id))
+            if res == None:
+                print("\nThere is no employee with that ID\n")
+                continue
+            print("\n",res)
+            print("\n---------------------------------------------\n")
 
-x = EMS()
+        if inpt == "3":
+            id = input("Enter the employee ID: ")
+            res = obj.deleteEmployeeInfo(int(id))
+            if res == None:
+                print("\nThere is no employee with that ID\n")
+                continue
+            print("\nEmployee succefully deleted!")
+            print("\n---------------------------------------------\n")
+
+        if inpt == "q":
+            break
+
+if __name__ == "__main__":
+    main()
