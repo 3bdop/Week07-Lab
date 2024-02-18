f931812995d4e8ce64c45f1edf24f4d3602061d6
abood1509 <abdbinm55@gmail.com>
2024-02-18 15:11:15 +0300

    third commit

--- unittest_EMS.py
+++ unittest_EMS.py
@@ -10,18 +10,18 @@ class TestEMS(unittest.TestCase):
     def test_add_employee(self):
         emp = Employee("Ali", 21, 1, "Software Engineer")
         self.ems.addNewEmployee("Ali", 21, "Software Engineer")
-        self.assertEqual(self.ems.getEmployeeInfo(1), emp)
+        self.assertEqual(self.ems.getEmployeeInfo(1), emp, True)
 
     def test_remove_employee(self):
         emp = Employee("Ali", 21, 1, "Software Engineer")
         self.ems.addNewEmployee("Ali", 21, "Software Engineer")
         self.ems.deleteEmployeeInfo(1)
-        self.assertIsNone(self.ems.getEmployeeInfo(1))
+        self.assertIsNone(self.ems.getEmployeeInfo(1), True)
 
     def test_get_employee(self):
         emp = Employee("Ali", 21, 1, "Software Engineer")
         self.ems.addNewEmployee("Ali", 21, "Software Engineer")
-        self.assertEqual(self.ems.getEmployeeInfo(1), emp)
+        self.assertEqual(self.ems.getEmployeeInfo(1), emp, True)
 
 
 if __name__ == "__main__":
