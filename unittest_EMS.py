65bb58d897d87e6e2d84509d1b84adc4fa2db1e1
abood1509 <abdbinm55@gmail.com>
2024-02-18 20:54:52 +0300

    unitest modefied

--- unittest_EMS.py
+++ unittest_EMS.py
@@ -8,20 +8,33 @@ class TestEMS(unittest.TestCase):
         self.ems = EMS()
 
     def test_add_employee(self):
-        emp = Employee("Ali", 21, 1, "Software Engineer")
-        self.ems.addNewEmployee("Ali", 21, "Software Engineer")
-        self.assertEqual(self.ems.getEmployeeInfo(1), emp, True)
+        emp = Employee("Ali", 21, 1, "IT")
+        self.ems.addNewEmployee("1", "Ali", "21", "IT")
+        added_emp = self.ems.getEmployeeInfo("1")
+        self.assertEqual(
+            (added_emp.name, added_emp.age, added_emp.id, added_emp.department),
+            (emp.name, emp.age, emp.id, emp.department),
+        )
 
     def test_remove_employee(self):
-        emp = Employee("Ali", 21, 1, "Software Engineer")
-        self.ems.addNewEmployee("Ali", 21, "Software Engineer")
-        self.ems.deleteEmployeeInfo(1)
-        self.assertIsNone(self.ems.getEmployeeInfo(1), True)
+        emp = Employee("Ali", 21, 1, "IT")
+        self.ems.addNewEmployee("1", "Ali", "21", "IT")
+        self.ems.deleteEmployeeInfo("1")
+        try:
+            self.ems.getEmployeeInfo("1")
+        except ValueError:
+            pass
+        else:
+            self.fail("ValueError not raised")
 
     def test_get_employee(self):
-        emp = Employee("Ali", 21, 1, "Software Engineer")
-        self.ems.addNewEmployee("Ali", 21, "Software Engineer")
-        self.assertEqual(self.ems.getEmployeeInfo(1), emp, True)
+        emp = Employee("Abood", 21, 3, "IT")
+        self.ems.addNewEmployee("3", "Abood", "21", "IT")
+        added_emp = self.ems.getEmployeeInfo("3")
+        self.assertEqual(
+            (added_emp.name, added_emp.age, added_emp.id, added_emp.department),
+            (emp.name, emp.age, emp.id, emp.department),
+        )
 
 
 if __name__ == "__main__":
